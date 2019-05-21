9c021e84a55cd53f8183f63ee475e828ac0c2ac0
Kassam <mustaphakassam@gmail.com>
2019-05-21 16:41:17 +0300

    toggle the opening the pane

--- application/views/homepanel.php
+++ application/views/homepanel.php
@@ -45,30 +45,43 @@
       </div>
     </div>
   </div>
-  <div class="vl-account-pane">
+  <div class="vl-account-pane" id="vl-account-pane">
     <div class="vl-close-btn-pane" id="vl-open-btn-holder">
-      <button id="vl-open-btn" type="button" name="button" class="btn vl-btn-outline">Open</button>
+      <button id="vl-open-btn" type="button" name="button" class="btn vl-btn-outline"><i class="fa fa-bars"></i> </button>
     </div>
     <div class="vl-content" id="vl-pane-control">
       <div class="container-fluid vl-hader-bar">
         <div class="row">
           <div class="col-md-2">
             <ul>
-              <li><button type="button" name="button" class="btn vl-btn-outline">Sign Up</button> </li>
+              <li><button id="vl-signup-open" type="button" name="button" class="btn vl-btn-outline">Sign Up</button> </li>
             </ul>
           </div>
           <div class="col-md-10">
             <div class="vl-info-list">
               <ul>
-                <li><button type="button" name="button" class="btn vl-btn-outline">Close</button> </li>
+                <li><button id="vl-signin-open" type="button" name="button" class="btn vl-btn-outline">Sign In</button> </li>
               </ul>
             </div>
           </div>
         </div>
       </div>
       <div class="col-md-4 container vl-form-wrapper" id="vl-form">
-        <h5>LOGIN</h5>
-        <form id="vl-login-form" class="" action="index.html" method="post">
+
+        <form id="vl-login-form" class="vl-all-open-form" action="index.html" method="post">
+          <h5>LOGIN</h5>
+          <div class="form-group">
+            <input placeholder="Email" type="text" class="form-control vl-input-underline" name="" value="">
+          </div>
+          <div class="form-group">
+            <input placeholder="Password" type="password" class="form-control vl-input-underline" name="" value="">
+          </div>
+          <div class="form-group">
+            <button type="submit" name="button" class="form-control vl-btn-login">Login</button>
+          </div>
+        </form>
+        <form id="vl-signup-form" class="vl-all-form" action="index.html" method="post">
+          <h5>SIGN UP</h5>
           <div class="form-group">
             <input placeholder="Email" type="text" class="form-control vl-input-underline" name="" value="">
           </div>
---
+++ assets/js/main_script.js
@@ -0,0 +1,4 @@
+$('#vl-open-btn').on('click', function(){
+  $('#vl-account-pane').toggleClass('vl-account-pane-open');
+  $('#vl-pane-control').toggleClass('vl-content-open');
+});
