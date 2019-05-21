a5319a6cf0438781a5974d342106e40ff975b235
Kassam <mustaphakassam@gmail.com>
2019-05-21 15:14:07 +0300

    designed tipd holder

---
+++ application/views/homepanel.php
@@ -0,0 +1,51 @@
+<?php defined('BASEPATH')OR exit('Error'); ?>
+<section>
+  <div class="vl-map-container-fluid">
+    <div class="vl-map-holder" id="vl-map-container"></div>
+  </div>
+  <div class="container-fluid vl-tips-holder">
+    <div class="col-md-8 container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="vl-card-holder">
+            <div class="vl-card">
+              <h6>How to driver?</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                 in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="vl-card-holder">
+            <div class="vl-card">
+              <h6>How to driver?</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                 in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="vl-card-holder">
+            <div class="vl-card">
+              <h6>How to driver?</h6>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                 in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-flui vl-account-pane">
+
+  </div>
+</section>
