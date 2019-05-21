5078a364153f353bc92a80fcdcfc1b97d03173e6
Kassam <mustaphakassam@gmail.com>
2019-05-21 13:06:08 +0300

    Create homectrl.php

---
+++ application/controllers/homectrl.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH')OR exit('Error');
+/**
+ * controller for home control
+ */
+class homectrl extends CI_controller
+{
+
+  function __construct()
+  {
+    // code...
+    parent::__construct();
+    $this->load->helper('url');
+  }
+}
+ ?>
