b1573ffd128fdf3d3183cbf9831af4c9f2f9b531
Kassam <mustaphakassam@gmail.com>
2019-05-21 16:18:53 +0300

    design the pane

--- application/views/homepanel.php
+++ application/views/homepanel.php
@@ -46,17 +46,39 @@
     </div>
   </div>
   <div class="vl-account-pane">
-    <div class="container-fluid vl-hader-bar">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="vl-info-list">
+    <div class="vl-close-btn-pane" id="vl-open-btn-holder">
+      <button id="vl-open-btn" type="button" name="button" class="btn vl-btn-outline">Open</button>
+    </div>
+    <div class="vl-content" id="vl-pane-control">
+      <div class="container-fluid vl-hader-bar">
+        <div class="row">
+          <div class="col-md-2">
             <ul>
-              <!-- <li class="pro-list-border"><i class="fa fa-user"></i Kassam Mustapha</li> -->
-              <li>Not a member ? <button type="button" name="button" class="btn vl-btn-outline">Sign Up</button> </li>
-              <li><button type="button" name="button" class="btn vl-btn-outline">Close</button> </li>
+              <li><button type="button" name="button" class="btn vl-btn-outline">Sign Up</button> </li>
             </ul>
           </div>
+          <div class="col-md-10">
+            <div class="vl-info-list">
+              <ul>
+                <li><button type="button" name="button" class="btn vl-btn-outline">Close</button> </li>
+              </ul>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="col-md-4 container vl-form-wrapper" id="vl-form">
+        <h5>LOGIN</h5>
+        <form id="vl-login-form" class="" action="index.html" method="post">
+          <div class="form-group">
+            <input placeholder="Email" type="text" class="form-control vl-input-underline" name="" value="">
+          </div>
+          <div class="form-group">
+            <input placeholder="Password" type="password" class="form-control vl-input-underline" name="" value="">
+          </div>
+          <div class="form-group">
+            <button type="submit" name="button" class="form-control vl-btn-login">Login</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
