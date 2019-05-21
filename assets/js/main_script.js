67e10e2996dd37bb8ca19cbfa1a118df4c52184c
Kassam <mustaphakassam@gmail.com>
2019-05-21 16:50:34 +0300

    control the signIn and signUp form open and close

--- assets/js/main_script.js
+++ assets/js/main_script.js
@@ -2,3 +2,13 @@ $('#vl-open-btn').on('click', function(){
   $('#vl-account-pane').toggleClass('vl-account-pane-open');
   $('#vl-pane-control').toggleClass('vl-content-open');
 });
+//open the signup
+$('#vl-signup-open').on('click', function(){
+  $('#vl-login-form').removeClass('vl-all-open-form').addClass('vl-all-form');
+  $('#vl-signup-form').removeClass('vl-all-form').addClass('vl-all-open-form');
+})
+//open the login
+$('#vl-signin-open').on('click', function(){
+  $('#vl-signup-form').removeClass('vl-all-open-form').addClass('vl-all-form');
+  $('#vl-login-form').removeClass('vl-all-form').addClass('vl-all-open-form');
+})
