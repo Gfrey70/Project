c8668c0d075dc087b56f277e6caeb8f679a39eee
Kassam <mustaphakassam@gmail.com>
2019-05-22 03:38:28 +0300

    design the dashboard for the system

--- application/views/user_dashboard.php
+++ application/views/user_dashboard.php
@@ -1,1 +1,145 @@
 <?php defined('BASEPATH')OR exit('Error'); ?>
+<body class="vl-bg-color">
+  <section>
+    <div class="col-md-10 container">
+      <div class="row">
+        <div class="col-md-4 vl-2-border">
+            <div class="vl-panel-nav-h">
+              <h6>USER LIST</h6>
+            </div>
+            <div class="vl-user-list-holder">
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+          <div class="">
+            <div class="vl-panel-nav">
+              <h6>USER PANEL</h6>
+              <ul>
+                <li><a href="#"><i class="fa fa-user"></i> User</a> </li>
+                <li><a href="#"><i class="fa fa-user"></i> Garage</a> </li>
+                <li><a href="#"><i class="fa fa-user"></i> Owner</a></li>
+                <li><a href="#"><i class="fa fa-user"></i> Tips</a> </li>
+              </ul>
+            </div>
+            <div class="vl-panel-body">
+              <h6>User details</h6>
+              <form class="" action="index.html" method="post">
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <input placeholder="First Name" ckass="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <input placeholder="First Name" ckass="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <input placeholder="First Name" ckass="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <input placeholder="First Name" ckass="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <input placeholder="First Name" ckass="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="form-group">
+                      <input placeholder="First Name" ckass="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
