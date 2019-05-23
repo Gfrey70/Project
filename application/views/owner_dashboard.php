267a4121ec63bdd77a43b8cc27f767bc95ed76f2
Kassam <mustaphakassam@gmail.com>
2019-05-23 03:46:08 +0300

    Create owner_dashboard.php

---
+++ application/views/owner_dashboard.php
@@ -0,0 +1,176 @@
+<?php defined('BASEPATH')OR exit('Error'); ?>
+<body class="vl-bg-color">
+  <section>
+    <div class="col-md-10 container">
+      <div class="row">
+        <div class="col-md-4 vl-2-border">
+            <div class="vl-panel-nav-h">
+              <h6>GARAGE OWNERS LIST</h6>
+            </div>
+            <div class="vl-user-list-holder">
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+              <div class="vl-user-card">
+                <ul>
+                  <li><span class="vl-name">Mustapha kassam</span></li>
+                  <li><span>07156256978, mustapha@gmail.com, p.o.box ilala</span> </li>
+                  <li><div class="badge badge-primary">ADMIN</div> </li>
+                </ul>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-8 vl-1-border">
+          <div class="">
+            <div class="vl-panel-nav">
+              <h6>GARAGE OWNERS PANEL</h6>
+              <ul>
+                <li><a href="<?php echo base_url('index.php/user/panel');?>"><i class="fa fa-user"></i> User</a> </li>
+                <li><a href="<?php echo base_url('index.php/garage/panel');?>"><i class="fa fa-user"></i> Garage</a> </li>
+                <li><a href="<?php echo base_url('index.php/garage/ownerpanel');?>"><i class="fa fa-user"></i> Owner</a></li>
+                <li><a href="<?php echo base_url('index.php/tips/panel');?>"><i class="fa fa-user"></i> Tips</a> </li>
+              </ul>
+            </div>
+            <div class="vl-panel-body">
+              <h6>Garage details</h6>
+              <form class="" action="index.html" method="post">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <input placeholder="Garage Name" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <textarea placeholder="Garage description" name="name" rows="8" cols="80" class="form-control"></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <input placeholder="Country" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <input placeholder="Mobile" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <input placeholder="Email" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-3">
+                    <div class="form-group">
+                      <input placeholder="Location" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="form-group">
+                      <input placeholder="City" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="form-group">
+                      <input placeholder="District" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                  <div class="col-md-3">
+                    <div class="form-group">
+                      <input placeholder="Street" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <input placeholder="Street" class="form-control" type="text" name="" value="">
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="button" class="form-control vl-btn-login vl-wt-clr">Save Garage</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
