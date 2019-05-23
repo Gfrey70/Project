195c91eec68115c2bb44bc01cc9c13f5d52bf215
Kassam <mustaphakassam@gmail.com>
2019-05-23 13:42:47 +0300

    display the user details on the pro-infor view file

--- application/views/pro_info.php
+++ application/views/pro_info.php
@@ -1,7 +1,14 @@
 <span class="vl-profile-tab">
   <ul>
-    <li><span class="badge badge-success"><i class="fa fa-user"></i> kassam Mustapha</span>  </li>
-    <li><span class="badge badge-success">admin</span> </li>
+    <li>
+      <span class="badge badge-success">
+        <i class="fa fa-user"></i>
+        <?php echo $this->session->userdata['dataprofile']['fname'].' '.$this->session->userdata['dataprofile']['lname'];?>
+      </span>
+    </li>
+    <li>
+      <span class="badge badge-success"><?php echo $this->session->userdata['dataprofile']['rolename'];?></span>
+    </li>
     <li><a href="<?php echo base_url('index.php/user/logout');?>" class="badge badge-danger"><i class="fa fa-sign-out"></i> Logout</a></li>
   </ul>
 </span>
