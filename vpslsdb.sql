9c83b569fc513a9d28d3512ba6195a6486e386c1
Kassam <mustaphakassam@gmail.com>
2019-05-23 16:05:47 +0300

    changes in database schema and sql file for the query

--- vpslsdb.sql
+++ vpslsdb.sql
@@ -18,34 +18,36 @@ USE `vpslsdb` ;
 -- Table `vpslsdb`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `vpslsdb`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `fname` VARCHAR(255) NULL,
-  `lname` VARCHAR(255) NULL,
-  `gender` VARCHAR(45) NULL,
-  `mobile` INT NULL,
-  `email` VARCHAR(255) NULL,
-  `password` VARCHAR(255) NULL,
-  `address` VARCHAR(45) NULL,
-  `rolename` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `fname` VARCHAR(255) NULL DEFAULT NULL,
+  `lname` VARCHAR(255) NULL DEFAULT NULL,
+  `gender` VARCHAR(45) NULL DEFAULT NULL,
+  `mobile` INT(11) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `address` VARCHAR(45) NULL DEFAULT NULL,
+  `rolename` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `vpslsdb`.`cargarages`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `vpslsdb`.`cargarages` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `gname` VARCHAR(45) NULL,
-  `gdescription` TEXT NULL,
-  `location` VARCHAR(45) NULL,
-  `city` VARCHAR(45) NULL,
-  `district` VARCHAR(45) NULL,
-  `street` VARCHAR(45) NULL,
-  `country` VARCHAR(45) NULL,
-  `Pnumber` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `users_id` INT NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `gname` VARCHAR(45) NULL DEFAULT NULL,
+  `gdescription` TEXT NULL DEFAULT NULL,
+  `location` VARCHAR(45) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NULL DEFAULT NULL,
+  `district` VARCHAR(45) NULL DEFAULT NULL,
+  `street` VARCHAR(45) NULL DEFAULT NULL,
+  `country` VARCHAR(45) NULL DEFAULT NULL,
+  `Pnumber` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `users_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_cargarages_users_idx` (`users_id` ASC) ,
   CONSTRAINT `fk_cargarages_users`
@@ -53,92 +55,44 @@ CREATE TABLE IF NOT EXISTS `vpslsdb`.`cargarages` (
     REFERENCES `vpslsdb`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `vpslsdb`.`Drivers`
+-- Table `vpslsdb`.`drivers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `vpslsdb`.`Drivers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `DName` VARCHAR(45) NULL,
-  `phnumber` VARCHAR(45) NULL,
-  `sex` VARCHAR(45) NULL,
-  `Country` VARCHAR(45) NULL,
-  `City` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `vpslsdb`.`drivers` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `fname` VARCHAR(45) NULL DEFAULT NULL,
+  `lname` VARCHAR(255) NOT NULL,
+  `pnumber` VARCHAR(45) NULL DEFAULT NULL,
+  `address` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `gender` VARCHAR(45) NULL DEFAULT NULL,
+  `country` VARCHAR(45) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `vpslsdb`.`LogSheets`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `vpslsdb`.`LogSheets` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `logusername` VARCHAR(45) NULL,
-  `date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `users_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_LogSheets_users1_idx` (`users_id` ASC) ,
-  CONSTRAINT `fk_LogSheets_users1`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `vpslsdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `vpslsdb`.`maintTips`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `vpslsdb`.`maintTips` (
-  `idmaintTips` INT NOT NULL AUTO_INCREMENT,
-  `TipsTitle` VARCHAR(100) NULL,
-  `TipsDesc` VARCHAR(200) NULL,
-  `doc` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `addedby` INT NOT NULL,
-  PRIMARY KEY (`idmaintTips`),
-  INDEX `fk_maintTips_users1_idx` (`addedby` ASC) ,
-  CONSTRAINT `fk_maintTips_users1`
-    FOREIGN KEY (`addedby`)
-    REFERENCES `vpslsdb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `vpslsdb`.`GpsLocations`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `vpslsdb`.`GpsLocations` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `LongTude` DOUBLE NULL,
-  `latTude` DOUBLE NULL,
-  `cargarages_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_GpsLocations_cargarages1_idx` (`cargarages_id` ASC) ,
-  CONSTRAINT `fk_GpsLocations_cargarages1`
-    FOREIGN KEY (`cargarages_id`)
-    REFERENCES `vpslsdb`.`cargarages` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `vpslsdb`.`carservicedrivers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `vpslsdb`.`carservicedrivers` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `doc` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `drivers_id` INT NOT NULL,
-  `cargarages_id` INT NOT NULL,
+  `drivers_id` INT(11) NOT NULL,
+  `cargarages_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_carservicedrivers_Drivers1_idx` (`drivers_id` ASC) ,
   INDEX `fk_carservicedrivers_cargarages1_idx` (`cargarages_id` ASC) ,
   CONSTRAINT `fk_carservicedrivers_Drivers1`
     FOREIGN KEY (`drivers_id`)
-    REFERENCES `vpslsdb`.`Drivers` (`id`)
+    REFERENCES `vpslsdb`.`drivers` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_carservicedrivers_cargarages1`
@@ -146,18 +100,19 @@ CREATE TABLE IF NOT EXISTS `vpslsdb`.`carservicedrivers` (
     REFERENCES `vpslsdb`.`cargarages` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `vpslsdb`.`garageservices`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `vpslsdb`.`garageservices` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `servicename` VARCHAR(45) NULL,
-  `servicedescription` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `servicename` VARCHAR(45) NULL DEFAULT NULL,
+  `servicedescription` VARCHAR(45) NULL DEFAULT NULL,
   `doc` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  `cargarages_id` INT NOT NULL,
+  `cargarages_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_garageservices_cargarages1_idx` (`cargarages_id` ASC) ,
   CONSTRAINT `fk_garageservices_cargarages1`
@@ -165,7 +120,67 @@ CREATE TABLE IF NOT EXISTS `vpslsdb`.`garageservices` (
     REFERENCES `vpslsdb`.`cargarages` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `vpslsdb`.`gpslocations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vpslsdb`.`gpslocations` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `LongTude` DOUBLE NULL DEFAULT NULL,
+  `latTude` DOUBLE NULL DEFAULT NULL,
+  `cargarages_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_GpsLocations_cargarages1_idx` (`cargarages_id` ASC) ,
+  CONSTRAINT `fk_GpsLocations_cargarages1`
+    FOREIGN KEY (`cargarages_id`)
+    REFERENCES `vpslsdb`.`cargarages` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `vpslsdb`.`logsheets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vpslsdb`.`logsheets` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `logusername` VARCHAR(45) NULL DEFAULT NULL,
+  `date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `users_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_LogSheets_users1_idx` (`users_id` ASC) ,
+  CONSTRAINT `fk_LogSheets_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `vpslsdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `vpslsdb`.`mainttips`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vpslsdb`.`mainttips` (
+  `idmaintTips` INT(11) NOT NULL AUTO_INCREMENT,
+  `TipsTitle` VARCHAR(100) NULL DEFAULT NULL,
+  `TipsDesc` VARCHAR(200) NULL DEFAULT NULL,
+  `doc` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `addedby` INT(11) NOT NULL,
+  PRIMARY KEY (`idmaintTips`),
+  INDEX `fk_maintTips_users1_idx` (`addedby` ASC) ,
+  CONSTRAINT `fk_maintTips_users1`
+    FOREIGN KEY (`addedby`)
+    REFERENCES `vpslsdb`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
