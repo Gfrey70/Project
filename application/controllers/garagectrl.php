60e0b8fecc2b42b0a8af7334a2506671610bfd9b
Kassam <mustaphakassam@gmail.com>
2019-05-23 03:48:00 +0300

    linking the page

--- application/controllers/garagectrl.php
+++ application/controllers/garagectrl.php
@@ -11,6 +11,12 @@ class garagectrl extends CI_controller
     parent::__construct();
     $this->load->helper('url');
   }
+  function ownerview()
+  {
+    $this->load->view('head');
+    $this->load->view('owner_dashboard');
+    $this->load->view('footer');
+  }
   function garageview()
   {
     $this->load->view('head');
