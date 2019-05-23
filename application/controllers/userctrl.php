c28cc6fd19acd2703e6f175cba00c0806121cb8d
Kassam <mustaphakassam@gmail.com>
2019-05-23 13:19:16 +0300

    Update userctrl.php

--- application/controllers/userctrl.php
+++ application/controllers/userctrl.php
@@ -10,6 +10,7 @@ class userctrl extends CI_controller
     // code...
     parent::__construct();
     $this->load->helper('url');
+    $this->load->model('Users');
   }
   function userview()
   {
@@ -17,5 +18,29 @@ class userctrl extends CI_controller
     $this->load->view('user_dashboard');
     $this->load->view('footer');
   }
+
+  function userLogin()
+  {
+    $email = $this->input->post('email');
+    $password = md5($this->input->post('password'));
+    $userData = $this->Users->userAuthenticates($email,$password);
+
+    if ($userData) {
+        $this->session->set_userdata('isSessionThere',true);
+        $this->session->set_userdata('dataprofile',$userData);
+        redirect('adminpanel/user');
+      }else{
+        $this->session->set_flashdata('error_message', 'Please Try again.');
+        redirect("go/home");
+    }
+
+  }
+
+  function userLogout()
+  {
+    $this->session->sess_destroy();
+		redirect('go/home','refresh');
+  }
+
 }
  ?>
