5bc173784dd03180e6fd0babbc6d129db2adf703
Kassam <mustaphakassam@gmail.com>
2019-05-23 13:02:25 +0300

    Update pro_info.php

--- application/views/pro_info.php
+++ application/views/pro_info.php
@@ -1,6 +1,7 @@
 <span class="vl-profile-tab">
   <ul>
     <li><i class="fa fa-user"></i> kassam Mustapha</li>
-    <li><a href="#" class="badge badge-primary"><i class="fa fa-sign-out"></i> Logout</a></li>
+    <li><span class="badge badge-success">admin</span> </li>
+    <li><a href="#" class="badge badge-danger"><i class="fa fa-sign-out"></i> Logout</a></li>
   </ul>
 </span>
