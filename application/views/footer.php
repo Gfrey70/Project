6c1d33bdcc39ee5df57162ce0b5f98174ff993f9
Kassam <mustaphakassam@gmail.com>
2019-05-24 16:38:01 +0300

    link the plugins

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,8 @@
 <footer>
-  <script type="text/javascript" src="<?=base_url();?>assets/plugins/jquery/dist/jquery.js"></script>
-  <script type="text/javascript" src="<?=base_url();?>assets/js/main_script.js"></script>
+  <script type="text/javascript" src="<?php echo base_url();?>assets/plugins/jquery/dist/jquery.js"></script>
+  <script type="text/javascript" src="<?php echo base_url();?>assets/plugins/bootstrap/dist/js/bootstrap.js"></script>
+  <script type="text/javascript" src="<?php echo base_url();?>assets/plugins/slick-carousel/slick/slick.js"></script>
+  <script type="text/javascript" src="<?php echo base_url();?>assets/js/main_script.js"></script>
 </footer>
 </body>
 </html>
--- application/views/head.php
+++ application/views/head.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title><?=PRO_TITLE?></title>
-    <link rel="stylesheet" href="<?=base_url();?>assets/plugins/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="<?=base_url();?>assets/plugins/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="<?=base_url();?>assets/css/main_style.css">
+    <title><?php echo PRO_TITLE?></title>
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/slick-carousel/slick/slick.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/main_style.css">
   </head>
   <body>
